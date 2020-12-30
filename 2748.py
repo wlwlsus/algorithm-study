666c1bec070343eb3e9102b5ce53280fd728b83c
wonjun <159632_@naver.com>
2020-12-30 19:50:46 +0900

    Commit Test

---
+++ 2748.py
@@ -0,0 +1,17 @@
+def fibonacci(n):
+
+  cache = [-1 for _ in range(n+1)]
+  def iterate(n):
+    if n < 2:
+      return n
+
+    if cache[n] != -1:
+      return cache[n]
+    
+    cache[n] = iterate(n-1) + iterate(n-2)
+    return cache[n]
+  
+  return iterate(n)
+
+
+print(fibonacci(int(input())))
