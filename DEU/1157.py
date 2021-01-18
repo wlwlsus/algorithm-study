63481003cf79e7c1775c2f13caf50a723cfa743a
wonjun <159632_@naver.com>
2021-01-18 23:37:04 +0900

    Add 1157.py

---
+++ DEU/1157.py
@@ -0,0 +1,16 @@
+N = input().upper() # 대문자 변환
+
+def solution(s):
+    li = [] 
+    for i in set(s):
+        li.append(s.count(i))
+
+    idx = [i for i,x in enumerate(li) if x == max(li)]
+
+    if len(idx)>1 :
+        print("?")
+    else:
+        print(list(set(s))[li.index(max(li))])
+
+
+solution(N)
